f7186e262a44d1e0b7a6a21af644220fb21728a4
Paula Leite <paula.t.leite@icloud.com>
2019-10-31 14:59:29 -0300

    Creation of a cell for the base shape and organization of files

---
+++ Model/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  GameOfLife
+//
+//  Created by Paula Leite on 31/10/19.
+//  Copyright © 2019 Paula Leite. All rights reserved.
+//
+
+import SceneKit
+
+// Creation of the base shape of the Game of Life
+class Cell: SCNShape {
+    
+}
