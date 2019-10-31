6a2c5bb60f973f5fbd42b6d88a2dc3b4d45694c9
Paula Leite <paula.t.leite@icloud.com>
2019-10-31 17:38:54 -0300

    the matrix is working and it's entered.

--- GameOfLife/Controller/GameViewController.swift
+++ GameOfLife/Controller/GameViewController.swift
@@ -26,23 +26,40 @@ class GameViewController: UIViewController {
         let camera = SCNCamera()
         let cameraNode = SCNNode()
         cameraNode.camera = camera
-        cameraNode.position = SCNVector3(x: 0.0, y: 0.0, z: 3.0)
+        cameraNode.position = SCNVector3(x: 5.0, y: 4.0, z: 25.0)
         
         // Adds light
         let light = SCNLight()
         light.type = .omni
         let lightNode = SCNNode()
         lightNode.light = light
-        lightNode.position = SCNVector3(x: 1.5, y: 1.5, z: 1.5)
+        lightNode.position = SCNVector3(x: 5.0, y: 5.0, z: -5.0)
         
-        // Adds cube
-        let cubeGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
-        let cubeNode = SCNNode(geometry: cubeGeometry)
+        // Adds grid
+        let size = 9
+        let half = Int(size/2)
+        var centerCube = SCNNode()
+        
+        for i in 0..<size {
+            for j in 0..<size {
+                let cubeGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+                let cubeNode = SCNNode(geometry: cubeGeometry)
+                cubeNode.position = SCNVector3(1.15 * Float(i), 1.15 * Float(j), 0)
+                
+                scene.rootNode.addChildNode(cubeNode)
+                
+                if i == half && j == half {
+                    centerCube = cubeNode
+                }
+                
+                let constraint = SCNLookAtConstraint(target: centerCube)
+                constraint.isGimbalLockEnabled = true
+                cameraNode.constraints = [constraint]
+            }
+        }
         
         scene.rootNode.addChildNode(cameraNode)
         scene.rootNode.addChildNode(lightNode)
-        scene.rootNode.addChildNode(cubeNode)
-        
     }
     
 }
