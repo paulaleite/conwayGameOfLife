7c20c55b9e22684f0b1d88cf5ec6eef9027d2368
Paula Leite <paula.t.leite@icloud.com>
2019-11-01 14:29:41 -0300

    Changed the code organization

--- GameOfLife/Controller/GameViewController.swift
+++ GameOfLife/Controller/GameViewController.swift
@@ -18,47 +18,27 @@ class GameViewController: UIViewController {
         guard let sceneView = self.view as? SCNView else { return }
         
         // Adds the Scene
-        let scene  = SCNScene()
+        let scene  = GameScene()
         sceneView.scene = scene
         
-        // Adds camera
-        let camera = SCNCamera()
-        let cameraNode = SCNNode()
-        cameraNode.camera = camera
-        cameraNode.position = SCNVector3(x: 5.0, y: 4.0, z: 25.0)
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tap(_:)))
+        sceneView.addGestureRecognizer(tapGesture)
         
-        // Adds light
-        let light = SCNLight()
-        light.type = .omni
-        let lightNode = SCNNode()
-        lightNode.light = light
-        lightNode.position = SCNVector3(x: 5.0, y: 5.0, z: -5.0)
+    }
+    
+    @objc func tap(_ gestureRecognizer: UIGestureRecognizer)  {
+        // Adds the view of the Scene
+        guard let sceneView = self.view as? SCNView else { return }
         
-        // Adds grid
-        let size = 9
-        let half = Int(size/2)
-        var centerCube = SCNNode()
+        // Finds the cube that was touched
+        let point = gestureRecognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(point, options: [:])
         
-        for i in 0..<size {
-            for j in 0..<size {
-                let cubeGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
-                let cubeNode = SCNNode(geometry: cubeGeometry)
-                cubeNode.position = SCNVector3(1.15 * Float(i), 1.15 * Float(j), 0)
-                
-                scene.rootNode.addChildNode(cubeNode)
-                
-                if i == half && j == half {
-                    centerCube = cubeNode
-                }
-                
-                let constraint = SCNLookAtConstraint(target: centerCube)
-                constraint.isGimbalLockEnabled = true
-                cameraNode.constraints = [constraint]
-            }
+        if hitResults.count > 0  {
+            let objectClicked = hitResults[0]
+            guard let node = objectClicked.node as? Cube else { return }
+            
         }
-        
-        scene.rootNode.addChildNode(cameraNode)
-        scene.rootNode.addChildNode(lightNode)
     }
     
 }
---
+++ GameOfLife/Model/Cube.swift
@@ -0,0 +1,29 @@
+//
+//  Cell.swift
+//  GameOfLife
+//
+//  Created by Paula Leite on 31/10/19.
+//  Copyright © 2019 Paula Leite. All rights reserved.
+//
+
+import SceneKit
+
+enum CubeState {
+    case alive
+    case dead
+}
+
+// Creation of the base shape of the Game of Life
+class Cube: SCNNode {
+    var state: CubeState = .dead
+    
+    override init() {
+        super.init()
+        self.geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
